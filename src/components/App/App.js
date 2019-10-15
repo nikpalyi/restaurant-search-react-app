6e7dcb3e0dfb0bb67c9dae80788a35920364eec2
nikpalyi <diavetito@gmail.com>
2019-10-15 14:54:58 +0100

    App updated with search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,11 +20,15 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className='App'>
-        <h1>Hungry</h1>
-        <SearchBar />
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
