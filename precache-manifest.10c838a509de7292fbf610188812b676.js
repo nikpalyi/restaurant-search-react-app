f37c9fc0ed4baafa98c5983f4b09e68123167c3a
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 13:38:00 +0000

    Updates

---
+++ precache-manifest.10c838a509de7292fbf610188812b676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f5b1911217e7fd0bb5551967427703",
+    "url": "/restaurant-search-react-app/index.html"
+  },
+  {
+    "revision": "b7e33681532f2b197388",
+    "url": "/restaurant-search-react-app/static/css/main.9fc57bfa.chunk.css"
+  },
+  {
+    "revision": "9341a6b12fe378310d23",
+    "url": "/restaurant-search-react-app/static/js/2.f9a948ae.chunk.js"
+  },
+  {
+    "revision": "b7e33681532f2b197388",
+    "url": "/restaurant-search-react-app/static/js/main.a4e7370c.chunk.js"
+  },
+  {
+    "revision": "3c236777c32c25586d13",
+    "url": "/restaurant-search-react-app/static/js/runtime~main.244ec87b.js"
+  },
+  {
+    "revision": "ad60a4927c18d36696f166ffb317a3c6",
+    "url": "/restaurant-search-react-app/static/media/background.ad60a492.jpg"
+  }
+]);
