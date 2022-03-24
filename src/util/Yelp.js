c1dc3a6f622a4353076ad90428f280fdbfab929a
nikpalyi <16964051+nikpalyi@users.noreply.github.com>
2022-03-24 19:07:45 +0000

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,7 +4,7 @@ const apiKey =
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`
